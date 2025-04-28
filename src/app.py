efa3074d54504b9312d6e1508756c8c38eb0a605
RiverByte9 <kajalsalgar909@gmail.com>
2025-04-28 12:54:17 +0530

    app file modified

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timezone
 import os
 
+
 app = Flask(__name__)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DB_LINK")
